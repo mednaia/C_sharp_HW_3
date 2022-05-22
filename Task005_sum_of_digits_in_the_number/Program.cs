5431dabc1e876e4dd6a4c1a56d9d692910b7f736
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 13:46:42 +0300

    Task 005 sum of digits Var.1 while_loop

---
+++ Task005_sum_of_digits_in_the_number/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Input A positive integer and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+int tmp = a;
+while (tmp > 0)
+{
+    sum = sum + tmp % 10;
+    tmp = tmp /10 ;
+}
+Console.Write($"The sum of digits of {a} is {sum}.");
