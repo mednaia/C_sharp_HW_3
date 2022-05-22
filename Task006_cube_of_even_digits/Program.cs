ffd342de47ea2128158fa476ac18cc0fab407cf5
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 14:17:42 +0300

    Task 006 cube of even digits

---
+++ Task006_cube_of_even_digits/Program.cs
@@ -0,0 +1,4 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Input positive integer and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+if(a%2==0) Console.Write(a*a*a);
