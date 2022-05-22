cce0518c913ac7834cdf5b4204f8a853723ccc8e
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 11:52:18 +0300

    Task 007 array 0 and 1

---
+++ Task007_array_0_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[] a=new int[8];
+Random rand = new Random();
+for (int i=0; i <8; i++)
+{
+    a[i] = rand.Next(0,2);
+    Console.Write(" " + a[i]);
+}
