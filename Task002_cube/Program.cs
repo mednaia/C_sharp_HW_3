6f53045d3b2e277960c432f981b0d667443f1d72
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 11:39:12 +0300

    Task 002 cube

---
+++ Task002_cube/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Input  positive integer and press Enter: ");
+int n = int.Parse(Console.ReadLine());
+for(int count = 1; count <= n; count++)
+{
+    Console.Write($"{count * count * count} ");
+}
