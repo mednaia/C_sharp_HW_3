ae25dd07d6548af99d0c1c6fb702808bb5ab768a
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 13:09:11 +0300

    Task004 power for_loop

---
+++ Task004_power/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.WriteLine("Input A positive integer (that you want to raise to a power) and press Enter: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input B positive integer (power of a number) and press Enter: ");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+for(int i=1;i<=b;i++)
+{
+    result *= a; // *= Оператор вначале умножает значение выражения (в правой части оператора) на значение переменной или свойства (в левой части оператора) (слева). Затем оператор присваивает результат этой операции переменной или свойству.
+}
+Console.Write($"Value {a} to the degree {b} is {result}.");
