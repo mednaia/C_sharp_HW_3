b3cf5fa6aca680323c71848ed515572ea6345096
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-22 13:51:08 +0300

    Task005 sum of digits Var.2 for_loop

--- Task005_sum_of_digits_in_the_number/Program.cs
+++ Task005_sum_of_digits_in_the_number/Program.cs
@@ -2,10 +2,8 @@
 Console.WriteLine("Input A positive integer and press Enter: ");
 int a = int.Parse(Console.ReadLine());
 int sum = 0;
-int tmp = a;
-while (tmp > 0)
+for (int tmp = a; tmp > 0; tmp = tmp /10)
 {
     sum = sum + tmp % 10;
-    tmp = tmp /10 ;
 }
 Console.Write($"The sum of digits of {a} is {sum}.");
