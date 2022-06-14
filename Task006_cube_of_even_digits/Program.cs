022450a1364e52358dcf54adbda31261459bde96
Ekaterina <tiurina.kate@gmail.com>
2022-06-14 20:46:21 +0300

    Task006 Cube ending in even digit

--- Task006_cube_of_even_digits/Program.cs
+++ Task006_cube_of_even_digits/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
-Console.WriteLine("Input positive integer and press Enter: ");
-int a = int.Parse(Console.ReadLine());
-if(a%2==0) Console.Write(a*a*a);
+// Console.WriteLine("Input positive integer and press Enter: ");
+// int a = int.Parse(Console.ReadLine());
+// if(a%2==0) Console.Write(a*a*a);
+
+//Показать кубы чисел, заканчивающиеся на чётную цифру
+Console.WriteLine("Input  positive integer and press Enter: ");
+int n = int.Parse(Console.ReadLine());
+for(int count = 1; count <= n; count++)
+{
+    int result = count * count * count;
+    if(result%2==0) Console.Write($"{result} ");
+}
